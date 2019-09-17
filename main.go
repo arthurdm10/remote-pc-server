9fcd924ec795cd2d82872b8a27dba258b9e2aace
arthurdm10 <arthurdemacq@hotmail.com>
2019-09-17 20:45:54 -0300

    updates

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func loadEnvVars() (string, string, string, string) {
 	mongoDbHost, found := os.LookupEnv("MONGODB_HOST")
 
 	if !found {
-		log.Printf("MONGODB_HOST environment variable not found, using default (mongo:27017)")
+		log.Printf("MONGODB_HOST not defined, using default (mongo:27017)")
 		mongoDbHost = "mongo:27017"
 	} else {
 		re := regexp.MustCompile(`(^[a-zA-z0-9\.]+:\d+)`)
@@ -26,7 +26,7 @@ func loadEnvVars() (string, string, string, string) {
 
 	port, found := os.LookupEnv("PORT")
 	if !found {
-		log.Printf("PORT environment variable not found, using default (9002)")
+		log.Printf("PORT not defined, using default (9002)")
 		port = "9002"
 	} else {
 		re := regexp.MustCompile(`\d+`)
@@ -40,13 +40,13 @@ func loadEnvVars() (string, string, string, string) {
 	to register a remote PC and users that will access this PC*/
 	adminUser, found := os.LookupEnv("ADMIN_USER")
 	if !found {
-		log.Printf("Required ADMIN_USER environment variable not found")
+		log.Printf("Required ADMIN_USER environment variable not defined")
 		os.Exit(1)
 	}
 
 	adminPassword, found := os.LookupEnv("ADMIN_PASSWORD")
 	if !found {
-		log.Printf("Required ADMIN_PASSWORD environment variable not found")
+		log.Printf("Required ADMIN_PASSWORD environment variable not defined")
 		os.Exit(1)
 	}
 
